16bfeb2656765bf59d7c6efe31a122f35ebf3f10
FirsovRG <firsvorg@gmail.com>
2020-02-02 21:33:31 +0300

    lootcheck-tdd

---
+++ src/reducers/balance.test.js
@@ -0,0 +1,32 @@
+import balanceReducer from "./balance";
+import * as constants from "../actions/constants";
+
+describe("balanceReducer", () => {
+  it("sets a balance", () => {
+    const balance = 10;
+
+    expect(
+      balanceReducer(undefined, { type: constants.SET_BALANCE, balance })
+    ).toEqual(balance);
+  });
+
+  it("deposits into the balance", () => {
+    const deposit = 10;
+
+    const initialState = 5;
+
+    expect(
+      balanceReducer(initialState, { type: constants.DEPOSIT, deposit })
+    ).toEqual(initialState + deposit);
+  });
+
+  it("withdraw from the balance", () => {
+    const withdraw = 5;
+
+    const initialState = 10;
+
+    expect(
+      balanceReducer(initialState, { type: constants.WITHDRAW, withdraw })
+    ).toEqual(initialState - withdraw);
+  });
+});
